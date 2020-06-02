1706cc3f51826f383d3615b3ea5af8e78e13deef
yusminura <43083275+yusminura@users.noreply.github.com>
2020-06-02 23:55:31 +0700

    Update dashboard.php

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,21 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	public function __construct()
-	{
+	function __construct(){
+
 		parent::__construct();
+		check_not_login();
+		$this->load->model(['M_transaksi', 'M_paket', 'M_dashboard']);
 
-		if($this->session->userdata('role_id') != '1'){
-			$this->session->set_flashdata('message','<div class="alert alert-danger alert-dismissible fade show" role="alert">Silahkan Login Dahulu<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-			redirect('auth/login');
-		}
 	}
-
+	
 	public function index()
 	{
-		$this->load->view('templates/header');
-		$this->load->view('templates/sidebar');
-		$this->load->view('v_dashboard');
-		$this->load->view('templates/footer');
+		check_not_login();
+		$data['count_baru'] = $this->M_dashboard->get_count_monthly();
+		$data['count_pegawai'] = $this->M_dashboard->get_count_pegawai();
+		$data['countdaily'] = $this->M_dashboard->get_count_daily();
+		$data['counttransaksi'] = $this->M_dashboard->get_count_transaksi();
+
+		$data['recent'] = $this->db->query("SELECT * FROM paket pk, transaksi tr  WHERE pk.id=tr.id_paket AND tgl_transaksi=CURRENT_DATE() ")->result();
+		//	var_dump($data);
+		//	die();
+		$this->template->load('template', 'dashboard', $data);
 	}
+
 }
